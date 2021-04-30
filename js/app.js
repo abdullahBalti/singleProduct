ab2dba2e2f6d7c6340672ace27e1c3dc098ef984
abdullahBalti <abdullahbalti.khan@yahoo.com>
2021-04-30 06:21:52 +0500

    changes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 
 const onClick = document.getElementById("onclick");
 const sideNav = document.querySelector(".side-nav");
+const sideClose = document.querySelector(".side-nav-close");
 
 onClick.addEventListener('click', function(){
 	// console.log(sideNav.classList);
 	sideNav.classList.toggle("show-side-nav");
+});
 
+sideClose.addEventListener('click', function(){
+	// alert('message?: DOMString');
+	sideNav.classList.remove("show-side-nav");
 });
